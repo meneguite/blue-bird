f799d509633ea7e54def1c08fc88052f8e65049e
Ronaldo Meneguite <ronaldo@fireguard.com.br>
2019-09-29 22:09:55 -0300

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,36 @@
+const version = 'v1.0.0';
+const urlsToCache = [`/`, `/icon-144x144.png`, `/blue-bird.js`];
+const staticCacheName = 'blue-bird-' + version;
+
+self.addEventListener('install', function(event) {
+  event.waitUntil( 
+    caches.open(staticCacheName).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => (cacheName.startsWith('blue-bird-')))
+          .filter(cacheName => (cacheName !== staticCacheName))
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('/');
+      })
+  )
+});
